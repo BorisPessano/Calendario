52da0f17cb70dc2b3a9c65f9d4fccab802e0d243
Matias Tafernaberry <gonzalo.tafernaberry@gmail.com>
2023-12-20 14:51:42 -0300

    agregando el codigo de login de boris

--- js/login.js
+++ js/login.js
@@ -1,7 +1,31 @@
-function Registrarse() {
-    var email = localStorage.getElementById('Email');
-    var pass = localStorage.getElementById('Password');
+function Login() {
+   
+    var email = document.getElementById('Email');
+    var pass  = document.getElementById('Password');
     
+    let body = {
+        email: email.value,
+        password: pass.value
+    }
+    console.log('intento de body: ', body)
     // Llamo al magico
+    fetch('http://localhost:6001/api/clevendario/user/login',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(body)
+
+    }).then(response => response.json())
+    .then(responseData => {
+        window.location.href = 'index.html';
+    })
+    .catch(error => {
+        console.error('Error al llamar al servicio:', error);
+    });
+    
+    
+    
     
 }
