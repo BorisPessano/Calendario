60d55898eb7f972e92eb393752854799790a9827
Claudio Russi <crussi@cleveritgroup.com>
2023-12-21 11:34:51 -0300

    Add holidays to the calendar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 var calendar;
 
+var holidays = [
+    { date: "2024-01-01", description: "Año Nuevo" },
+    { date: "2024-02-12", description: "Carnaval" },
+    { date: "2024-02-13", description: "Carnaval" },
+    { date: "2024-03-28", description: "Semana de turismo" },
+    { date: "2024-03-29", description: "Semana de turismo" },
+    { date: "2024-04-22", description: "Desembarco de los 33. Feriado corrido de fecha" },
+    { date: "2024-05-01", description: "Día de los trabajadores" },
+    { date: "2024-06-19", description: "Natalicio de Artigas" },
+    { date: "2024-07-18", description: "Jura de la Constitución" },
+    { date: "2024-12-25", description: "Navidad" },
+    { date: "2023-12-25", description: "Navidad" },
+];
+
 function marcarAsistencia() {
     
     var email = document.getElementById('email');
@@ -34,6 +48,36 @@ function marcarAsistencia() {
     mostrarRegistro();
 }
 
+function calculateRemainingDays(presentDays) {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth();
+    const lastDay = new Date(year, month + 1, 0);
+    const workDays = lastDay.getDate();
+    const presentDaysCount = presentDays.length;
+    const presentPercentage = (presentDaysCount / workDays) * 100;
+
+    if (presentPercentage < 60) {
+        const remainingDays = Math.ceil((0.6 * workDays) - presentDaysCount);
+        alert(`Te faltan ${remainingDays} días más de asistencia a la oficina`);
+    }
+}
+
+function mostrarFeriados() {
+    holidays.filter(val => {
+        var date = new Date(val.date);
+        return date.getMonth() === new Date().getMonth() && date.getYear() === new Date().getYear();
+    }).forEach(val => {
+        var date = new Date(val.date);
+        calendar.addEvent({
+            id: date,
+            title: "Feriado",
+            start: date.toISOString().split('T')[0],
+            backgroundColor: "blue"
+        });
+    })
+}
+
 function mostrarRegistro() {
     var registro = document.getElementById('registro') || '[]';
     var email = document.getElementById('email');
@@ -48,20 +92,20 @@ function mostrarRegistro() {
 
     }).then(response => response.json())
     .then(responseData => {
-        if (responseData.length > 0) {
-            responseData.forEach(function (entrada) {
-                var date = new Date(entrada.createdAt);
-                console.log(entrada);
-                if (date.getMonth() === new Date().getMonth() && date.getYear() === new Date().getYear()) {
-                    calendar.addEvent({
-                        id: entrada.createdAt,
-                        title: 'Feriado',
-                        start: date,
-                        backgroundColor: "red"
-                    });
-                }
+        var filteredData = responseData.filter(val => {
+            var date = new Date(val.createdAt);
+            return date.getMonth() === new Date().getMonth() && date.getYear() === new Date().getYear();
+        });
+        calculateRemainingDays(filteredData);
+        filteredData.forEach(function (entrada) {
+            var date = new Date(entrada.createdAt);
+            calendar.addEvent({
+                id: entrada.createdAt,
+                title: entrada.action.name === "REMOTE" ? "Remoto" : "Presencial",
+                start: date.toISOString().split('T')[0],
+                backgroundColor: entrada.action.name === "REMOTE" ? "red" : "green"
             });
-        }
+        });
     })
     .catch(error => {
         console.error('Error al llamar al servicio:', error);
@@ -87,5 +131,6 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
     calendar.render();
+    mostrarFeriados();
     mostrarRegistro();
   });
