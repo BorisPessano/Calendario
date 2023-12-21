40b10fba8f862ac022bfa484782d2fa6a08db57c
mnacimento <31542315+mnacimento@users.noreply.github.com>
2023-12-21 12:13:01 -0300

    WIP login control and logout clean cookies

--- js/index.js
+++ js/index.js
@@ -117,3 +117,14 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     return null;
   }
+
+  function limpiarCookies() {
+    let cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var igualPos = cookie.indexOf("=");
+        var nombre = igualPos > -1 ? cookie.substr(0, igualPos) : cookie;
+        document.cookie = nombre + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+}
