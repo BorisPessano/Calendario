381d28e25c5a0243ae3a7fab2dff051dfc4eec61
BorisPessano <bpessano@cleveritgroup.com>
2023-12-21 10:56:15 -0300

    added logic to check if are logged

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ function Login() {
         email: email.value,
         password: pass.value
     }
-    console.log('intento de body: ', body)
+    
     // Llamo al magico
     fetch('http://localhost:6001/api/clevendario/user/login',{
         method: 'POST',
@@ -19,6 +19,7 @@ function Login() {
 
     }).then(response => response.json())
     .then(responseData => {
+        establecerCookie('token',responseData.access_token,3600);
         window.location.href = 'index.html';
     })
     .catch(error => {
@@ -29,3 +30,17 @@ function Login() {
     
     
 }
+
+function establecerCookie(nombre, valor, expiracion) {
+    const fechaExpiracion = new Date();
+    fechaExpiracion.setTime(fechaExpiracion.getTime() + expiracion * 1000); // expiracion en segundos
+
+    const opciones = {
+      expires: fechaExpiracion.toUTCString(),
+      path: '/', // Ruta para la cual la cookie es válida
+      sameSite: 'strict', // Restringir la cookie a solicitudes del mismo sitio
+      secure: window.location.protocol === 'https:', // Solo enviar la cookie en conexiones seguras (https)
+    };
+
+    document.cookie = `${nombre}=${valor}; ${Object.entries(opciones).map(([key, value]) => `${key}=${value}`).join('; ')}`;
+  }
